dc5666da67a489a34b1ea25d21a6d04153313efe
ttdesign <tkthedesigner@gmail.com>
2021-12-07 22:24:29 -0800

    fetch data from api with axios

---
+++ utils/fetchAPI.js
@@ -0,0 +1,13 @@
+import axios  from "axios";
+export const baseUrl='https://bayut.p.rapidapi.com'
+
+
+
+export const fetchApi = async (url) =>{
+const {data} = await axios.get(url, {
+	headers: {
+		'x-rapidapi-host': 'bayut.p.rapidapi.com',
+		'x-rapidapi-key': '651f129c5dmsh77b3f9006c2b46fp169f6ajsn82622810010b',
+	},
+});
+}
