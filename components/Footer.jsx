d8096d7466cd307895acd91ddc476ede38200c40
ttdesign <tkthedesigner@gmail.com>
2021-12-08 22:41:58 -0800

    add navigation and footer component, render

---
+++ components/Footer.jsx
@@ -0,0 +1,9 @@
+import {Box} from '@chakra-ui/react'
+
+const Footer =() => (
+<Box textAlign='center' padding='5' color='gray.600' borderTop='1px' borderColor = 'gray.100'> 
+House Finder 2021, Inc. All Right Reserved, Done by TTiDesign
+</Box>
+)
+
+export default Footer
--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,8 +1,8 @@
 import Head from 'next/head';
 import {Box} from '@chakra-ui/react';
 
-
-
+import NavBar from './Navbar';
+import Footer from './Footer';
 const Layout = ({children}) => (
 <>
     <Head>
@@ -10,13 +10,13 @@ const Layout = ({children}) => (
     </Head>
     <Box maxWidth='1280px' m='auto'>
         <header>
-            Navbar
+            <NavBar/>
         </header>
         <main>
             {children}
         </main>
         <footer>
-            Footer
+            <Footer/>
         </footer>
     </Box>
 </>
