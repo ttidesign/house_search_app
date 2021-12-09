0872714a9dd06af7c5e015aa7e0df4bab6d9b93b
ttdesign <tkthedesigner@gmail.com>
2021-12-08 20:51:14 -0800

    add Property component, install react icons

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Link from 'next/link';
 import Image from 'next/image';
 import {Flex, Box, Text, Button} from '@chakra-ui/react'
 import { baseUrl, fetchApi } from '../utils/fetchAPI';
+import { Property } from '../components/Property'
 
 import styles from '../styles/Home.module.css'
 const Banner = ({ purpose, title1, title2, desc1, desc2, buttonText, linkName, imageUrl }) => (
@@ -33,7 +34,7 @@ export default function Home({propertiesForSale, propertiesForRent}) {
 					imageUrl='https://bayut-production.s3.eu-central-1.amazonaws.com/image/145426814/33973352624c48628e41f2ec460faba4'
 				/>
 				<Flex flexWrap='wrap'>
-					{/* fetch the properties and display here */}
+						{propertiesForRent.map((property) => <Property property = {property} key ={property.id}/>)}
 				</Flex>
 				<Banner
 					purpose='BUY A HOME'
@@ -46,7 +47,7 @@ export default function Home({propertiesForSale, propertiesForRent}) {
 					imageUrl='https://bayut-production.s3.eu-central-1.amazonaws.com/image/110993385/6a070e8e1bae4f7d8c1429bc303d2008'
 				/>
 				<Flex flexWrap='wrap'>
-					{/* fetch the properties and display here */}
+					{propertiesForSale.map((property) => <Property property = {property} key ={property.id}/>)}
 				</Flex>
 			</Box>
 		</div>
